16b3872f995ea3f5de8d5928adf6b6234a0807d0
Lukas <lukas.hofmann77@gmail.com>
2020-10-20 14:39:29 +0200

    Proper removal

--- app/src/main/java/com/example/dice/MainActivity.kt
+++ app/src/main/java/com/example/dice/MainActivity.kt
@@ -14,26 +14,53 @@ class MainActivity : AppCompatActivity() {
     private lateinit var timer: Timer
 
     private var time = 0
-    private val dieResources = listOf(R.drawable.d6__1, R.drawable.d6__2, R.drawable.d6__3, R.drawable.d6__4, R.drawable.d6__5, R.drawable.d6__6)
+    private val dieResources = listOf(
+        R.drawable.d6__1,
+        R.drawable.d6__2,
+        R.drawable.d6__3,
+        R.drawable.d6__4,
+        R.drawable.d6__5,
+        R.drawable.d6__6
+    )
     private val visibility = mutableListOf(true, false, false, false, false, false)
     private lateinit var dice: List<ImageView>
-    private lateinit var clickedDie: ImageView
 
-    private val recents = listOf<MutableList<Int>>(mutableListOf(), mutableListOf(), mutableListOf(), mutableListOf(), mutableListOf(), mutableListOf())
+    private val recents = listOf<MutableList<Int>>(
+        mutableListOf(),
+        mutableListOf(),
+        mutableListOf(),
+        mutableListOf(),
+        mutableListOf(),
+        mutableListOf()
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        dice = listOf(binding.ivDie1, binding.ivDie2, binding.ivDie3, binding.ivDie4, binding.ivDie5, binding.ivDie6)
+        dice = listOf(
+            binding.ivDie1,
+            binding.ivDie2,
+            binding.ivDie3,
+            binding.ivDie4,
+            binding.ivDie5,
+            binding.ivDie6
+        )
 
         setVisibility()
 
         binding.bRoll.setOnClickListener {
             if (this::timer.isInitialized) timer.cancel()
             time = 5
-            val results = listOf((0..5).random(), (0..5).random(), (0..5).random(), (0..5).random(), (0..5).random(), (0..5).random())
-            binding.tvResult.text = "Results: $results"
+            val results = listOf(
+                (0..5).random(),
+                (0..5).random(),
+                (0..5).random(),
+                (0..5).random(),
+                (0..5).random(),
+                (0..5).random()
+            )
+            binding.tvResult.text = "Result: $results"
             timer = fixedRateTimer("timer", false, 0L, 50) {
                 runOnUiThread {
                     if (setImg(results) == 0) {
@@ -57,13 +84,15 @@ class MainActivity : AppCompatActivity() {
     private fun setVisibility() {
         for ((x, die) in dice.withIndex()) {
             if (visibility[x]) {
-                die.setImageResource(dieResources[5])
                 die.visibility = View.VISIBLE
                 die.setOnClickListener {
                     binding.clPopup.visibility = View.VISIBLE
                     addOrRemove(die)
                 }
-            } else die.visibility = View.GONE
+            } else {
+                die.visibility = View.GONE
+                die.setImageResource(dieResources[5])
+            }
         }
     }
 
@@ -73,9 +102,26 @@ class MainActivity : AppCompatActivity() {
             binding.clPopup.visibility = View.GONE
         }
         binding.tvRemove.setOnClickListener {
-            visibility[dice.indexOf(selectedDie)] = false
+            val currentIndex = dice.indexOf(selectedDie)
+            var lastVisibleIndex = 5
+            for (i in visibility.indices) {
+                if (!visibility[i]) {
+                    lastVisibleIndex = i - 1
+                    println(lastVisibleIndex)
+                    break
+                }
+            }
+            val steps = lastVisibleIndex - currentIndex
+            for (i in 0 until steps) {
+                println(i)
+                dice[currentIndex + i].setImageDrawable(dice[currentIndex + i + 1].drawable)
+            }
             binding.clPopup.visibility = View.GONE
+            visibility[lastVisibleIndex] = false
             setVisibility()
+        }
+        binding.bExit.setOnClickListener {
+            binding.clPopup.visibility = View.GONE
         }
     }
 
