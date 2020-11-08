1ba6bd1f68780631d4d9d26c25a9ea630ca41fe0
Lukas <lukas.hofmann77@gmail.com>
2020-11-08 05:59:16 +0100

    Added clickable GitHub link

--- app/src/main/java/com/example/dice/about/AboutFragment.kt
+++ app/src/main/java/com/example/dice/about/AboutFragment.kt
@@ -1,17 +1,28 @@
 package com.example.dice.about
 
 import android.os.Bundle
+import android.text.method.LinkMovementMethod
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.example.dice.R
+import com.example.dice.databinding.FragmentAboutBinding
 
 class AboutFragment : Fragment() {
+    private lateinit var binding: FragmentAboutBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_about, container, false)
+        binding = DataBindingUtil.inflate(layoutInflater, R.layout.fragment_about, container, false)
+        setupHyperlink()
+        return binding.root
+    }
+
+    private fun setupHyperlink() {
+        binding.aboutMeText.movementMethod = LinkMovementMethod.getInstance()
     }
 }
